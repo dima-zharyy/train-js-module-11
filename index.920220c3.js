5367e217fcea2c89895195a81a07a9ac0ad531ff
dima-zharyy <93909777+dima-zharyy@users.noreply.github.com>
2022-06-05 20:04:32 +0000

    Deploying to gh-pages from @ dima-zharyy/train-js-module-11@02f541d2afcab811ca66107f99fc3d24d71b9d4d 🚀

---
+++ index.920220c3.js
@@ -0,0 +1,2 @@
+!function(){function t(t){return fetch("".concat("http://localhost:3000","/books/").concat(t),{method:"DELETE"}).then((function(t){return t.json()}))}t(1),t(2),t(3)}();
+//# sourceMappingURL=index.920220c3.js.map
---
+++ index.a5a582ca.js
@@ -0,0 +1,2 @@
+function t(t){return fetch(`http://localhost:3000/books/${t}`,{method:"DELETE"}).then((t=>t.json()))}t(1),t(2),t(3);
+//# sourceMappingURL=index.a5a582ca.js.map
